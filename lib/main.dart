629ce608829e409cfef5f7e814c8dec9ce4e99f7
fwdAndroid <fwdkaleem@gmail.com>
2023-05-18 11:13:54 +0500

    FIrebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:osar_user/introscreens/welcome.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
